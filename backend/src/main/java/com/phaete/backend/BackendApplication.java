ce7221f1ea6853cffef072ca10eef531de03436d
-St4n <pel4nk@gmail.com>
2024-09-30 16:00:55 +0200

    Change backend artifact name

--- backend/src/main/java/com/phaete/backend/BackendApplication.java
+++ backend/src/main/java/com/phaete/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.phaete.fullstackbackend;
+package com.phaete.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/phaete/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/phaete/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.phaete.fullstackbackend;
+package com.phaete.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
